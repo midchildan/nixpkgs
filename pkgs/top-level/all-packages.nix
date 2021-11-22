bae7cf37a98fc3fa43ba178e654562ed4657a64f
midchildan <git@midchildan.org>
2021-11-22 18:19:10 +0900

    llvmPackages_13.libcxx: fix darwin build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12550,6 +12550,8 @@ with pkgs;
     targetLlvmLibraries = targetPackages.llvmPackages_13.libraries;
   } // lib.optionalAttrs (stdenv.hostPlatform.isi686 && buildPackages.stdenv.cc.isGNU) {
     stdenv = gcc7Stdenv;
+  } // lib.optionalAttrs stdenv.hostPlatform.isDarwin {
+    stdenv = llvmPackages_12.stdenv;
   }));
 
   llvmPackages_latest = llvmPackages_13;
